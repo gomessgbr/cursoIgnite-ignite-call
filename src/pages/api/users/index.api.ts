7fac66dafbfe0841fe380a0297cd4610c5cb2c63
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-14 06:01:19 -0300

    feat: save cookies to get id user without login

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { setCookie } from 'nookies'
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -27,6 +29,10 @@ export default async function handler(
       name,
       username,
     },
+  })
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
