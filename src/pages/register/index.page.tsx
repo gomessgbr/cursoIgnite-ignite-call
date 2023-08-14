ef5b003b44437784ebe8392d8ff4469348bb0dab
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-14 05:22:57 -0300

    feat: setting username

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -4,6 +4,8 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -24,6 +26,7 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
@@ -32,6 +35,13 @@ export default function Register() {
       name: '',
     },
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username)
+      setValue('username', String(router.query.username))
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
