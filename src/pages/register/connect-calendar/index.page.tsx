d58e0741ba35fd4bd5169356ce9b8ba8de639c05
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-14 19:52:02 -0300

    feat: calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,33 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight } from 'phosphor-react'
+
+import { api } from '@/lib/axios'
+import { ConnectBox, ConnectItem } from './style'
+
+export default function ConnectCalendar() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
