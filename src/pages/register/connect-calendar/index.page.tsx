9c1822d5b4d37a137fc90dd5af600f7b1746d19f
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-08-21 20:54:28 -0300

    feat: add create session

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,61 +1,77 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
-import { Container, Header } from '../styles'
+import { signIn, useSession } from 'next-auth/react'
+
+import { useRouter } from 'next/router'
 import { ArrowRight, Check } from 'phosphor-react'
 
-import { AuthError, ConnectBox, ConnectItem } from './style'
-import { signIn, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
 
 export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
   const hasAuthError = !!router.query.error
-  const isSignedIn = (session.status = 'authenticated')
+  const isSignedId = session.status === 'authenticated'
 
   async function handleConnectCalendar() {
     await signIn('google')
   }
 
-  return (
-    <Container>
-      <Header>
-        <Heading as="strong">Conecte sua agenda!</Heading>
-        <Text>
-          Conecte seu calendário para verificar automaticamente as horas
-          ocupadas e os novos eventos à medida em que são agendados
-        </Text>
-        <MultiStep size={4} currentStep={2} />
-      </Header>
-      <ConnectBox>
-        <ConnectItem>
-          <Text>Google Calendar</Text>
-          {isSignedIn ? (
-            <Button size="sm" disabled>
-              Conectado
-              <Check />
-            </Button>
-          ) : (
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={handleConnectCalendar}
-            >
-              Conectar <ArrowRight />
-            </Button>
-          )}
-        </ConnectItem>
-        {hasAuthError && (
-          <AuthError size="sm">
-            Falha ao se conectar ao Google, verifique se você habilitou as
-            permissões de acesso ao Google Calendar.
-          </AuthError>
-        )}
+  async function handleNavigateToNextStep() {
+    await router.push('/register/time-intervals')
+  }
 
-        <Button type="submit" disabled={!isSignedIn}>
-          Próximo passo <ArrowRight />
-        </Button>
-      </ConnectBox>
-    </Container>
+  return (
+    <>
+      <Container>
+        <Header>
+          <Heading as="strong">Conecte sua agenda!</Heading>
+          <Text>
+            Conecte o seu calendário para verificar automaticamente as horas
+            ocupadas e os novos eventos à medida em que são agendados.
+          </Text>
+
+          <MultiStep size={4} currentStep={2} />
+        </Header>
+
+        <ConnectBox>
+          <ConnectItem>
+            <Text>Google Calendar</Text>
+            {isSignedId ? (
+              <Button size="sm" disabled>
+                Conectado
+                <Check />
+              </Button>
+            ) : (
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={handleConnectCalendar}
+              >
+                Conectar
+                <ArrowRight />
+              </Button>
+            )}
+          </ConnectItem>
+
+          {hasAuthError && (
+            <AuthError size="sm">
+              Falha ao se conectar ao Google, verifique se você habilitou as
+              permissões de acesso ao Google Calendar
+            </AuthError>
+          )}
+
+          <Button
+            onClick={handleNavigateToNextStep}
+            type="submit"
+            disabled={!isSignedId}
+          >
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </ConnectBox>
+      </Container>
+    </>
   )
 }
